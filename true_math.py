48689402217e6dc982da31eb9a00460b553b3c9e
Mitchy-art <mari.rosolovich@inbox.ru>
2024-08-10 23:03:20 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,10 @@
+import math
+
+
+def divide(first, second):
+    if second != 0:
+        result = first/second
+        return result
+    if second == 0:
+        from math import inf
+        return inf
