a2df5655ef68792ed22071d5ef91277ec0a613f3
Mitchy-art <mari.rosolovich@inbox.ru>
2024-09-23 22:55:57 +0300

    Add files via upload

---
+++ module 7 4.py
@@ -0,0 +1,14 @@
+print('В команде Мастера кода участников: %(team1_num)s!' % {'team1_num':5})
+print('Итого сегодня в командах участников: %(team1_num)s и %(team2_num)s!' % {'team1_num':5,'team2_num':6})
+
+print('Команда Волшебники данных решила задач: {score_2}!'.format(score_2=42))
+print('Волшебники данных решили задачи за {team1_time} c!'.format(team1_time=18015.2, team2_time=2153.31451))
+
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result = 'победа команды Мастера кода!'
+print(f'Результат битвы: {challenge_result}')
+tasks_total = 82
+time_avg = 350.4
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
