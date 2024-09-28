ab7e4bf8882c92421f3daeeb80c1551972bf1e5b
Mitchy-art <mari.rosolovich@inbox.ru>
2024-09-28 19:20:53 +0300

    module 9 1.py

--- module 9 1.py
+++ module 9 1.py
@@ -1,19 +1,19 @@
-# рабочее решение
-# def apply_all_func(int_list, *functions):
-#     results = {}
-#     for function in functions:
-#         result = function(int_list)
-#         results[function.__name__] = result
-#     return results
-
-# нерабочее решение
+#рабочее решение
 def apply_all_func(int_list, *functions):
     results = {}
-    for number in int_list:
-        for function in functions:
-            result = map(function, number)
-            results[function.__name__] = result
-     return results
+    for function in functions:
+        result = function(int_list)
+        results[function.__name__] = result
+    return results
+
+# # нерабочее решение
+# def apply_all_func(int_list, *functions):
+#     results = {}
+#     for number in int_list:
+#         for function in functions:
+#             result = map(function, number)
+#             results[function.__name__] = result
+#      return results
 
 
 print(apply_all_func([6, 20, 15, 9], max, min))
