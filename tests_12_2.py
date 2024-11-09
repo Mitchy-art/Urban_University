bbef7526f2a2ac2e371d4427688ee359e4ed0d67
Mitchy-art <mari.rosolovich@inbox.ru>
2024-11-09 16:01:29 +0300

    tests_12_2.py

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,46 +1,6 @@
 from unittest import TestCase
 import unittest
-
-
-class Runner:
-    def __init__(self, name, speed=5):
-        self.name = name
-        self.distance = 0
-        self.speed = speed
-
-    def run(self):
-        self.distance += self.speed * 2
-
-    def walk(self):
-        self.distance += self.speed
-
-    def __str__(self):
-        return self.name
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == other
-        elif isinstance(other, Runner):
-            return self.name == other.name
-
-
-class Tournament:
-    def __init__(self, distance, *participants):
-        self.full_distance = distance
-        self.participants = list(participants)
-
-    def start(self):
-        finishers = {}
-        place = 1
-        while self.participants:
-            for participant in self.participants:
-                participant.run()
-                if participant.distance >= self.full_distance:
-                    finishers[place] = participant
-                    place += 1
-                    self.participants.remove(participant)
-
-        return finishers
+import runner_and_tournament as rt
 
 
 class TournamentTest(TestCase):
@@ -49,38 +9,46 @@ class TournamentTest(TestCase):
         cls.all_results = {}
 
     def setUp(self):
-        self.run_1 = Runner('Усэйн', 10)
-        self.run_2 = Runner('Андрей', 9)
-        self.run_3 = Runner('Ник', 3)
+        self.run_1 = rt.Runner('Усэйн', 10)
+        self.run_2 = rt.Runner('Андрей', 9)
+        self.run_3 = rt.Runner('Ник', 3)
 
     @classmethod
     def tearDownClass(cls):
         dict_fin = {}
         for key, value in cls.all_results.items():
+            print(f'Тест: {key}')
             for k, v in value.items():
-                dict_fin[k] = str(v)
-                print(value)
+                print(f'\t{k}: {v.name}')
+                # dict_fin[k] = v.name
+                # print(value)
 
     def test_Us_Nik(self):
-        tour_1 = Tournament(90, self.run_1, self.run_3)
+        tour_1 = rt.Tournament(90, self.run_1, self.run_3)
         results = tour_1.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(results[2] == "Ник")
+        self.assertTrue(results[list(results.keys())[-1]] == 'Ник')
+        self.all_results['test_turn1'] = results
+        # TournamentTest.all_results.update(results)
+        # self.assertTrue(results[2] == "Ник")
 
     def test_An_Nik(self):
-        tour_2 = Tournament(90, self.run_2, self.run_3)
+        tour_2 = rt.Tournament(90, self.run_2, self.run_3)
         results = tour_2.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(results[2] == "Ник")
+        self.assertTrue(results[list(results.keys())[-1]] == 'Ник')
+        self.all_results['test_turn2'] = results
+        # TournamentTest.all_results.update(results)
+        # self.assertTrue(results[2] == "Ник")
 
         # result = self.all_results
         # self.assertTrue(result[max(self.all_results.keys())] == "Ник")
 
     def test_Us_An_Nik(self):
-        tour_3 = Tournament(90, self.run_1, self.run_2, self.run_3)
+        tour_3 = rt.Tournament(90, self.run_1, self.run_2, self.run_3)
         results = tour_3.start()
-        TournamentTest.all_results.update(results)
-        self.assertTrue(results[3] == "Ник")
+        self.assertTrue(results[list(results.keys())[-1]] == 'Ник')
+        self.all_results['test_turn3'] = results
+        # TournamentTest.all_results.update(results)
+        # self.assertTrue(results[3] == "Ник")
 
 
 if __name__ == '__main__':
