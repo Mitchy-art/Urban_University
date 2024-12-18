f80f7c0d5d37bf5dbaad639910a771bc66228701
Mitchy-art <mari.rosolovich@inbox.ru>
2024-12-18 17:21:06 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -10,13 +10,13 @@ async def admin() -> dict:
 
 
 @app.get('/user/{user_id}')
-async def id_user(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=1)) -> dict:
+async def id_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=1)]) -> dict:
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user/{username}/{age}')
-async def user_name_age(username: str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
-                        age: int = Path(ge=18, le=120, description='Enter username', example='24')) -> dict:
+async def user_name_age(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                        age: Annotated[int, Path(ge=18, le=120, description='Enter username', example='24')]) -> dict:
     return {'message': f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
 
